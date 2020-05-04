cfe57c8fe5939720b8d42104134334b804dc1df9
jim <chuang.jim@baiyitech.com.tw>
2020-05-04 16:45:33 +0800

    sync

---
+++ do.php
@@ -0,0 +1,14 @@
+<?php
+
+include 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// echo $s3_bucket = getenv('S3_BUCKET');
+// echo $s3_bucket = $_ENV['S3_BUCKET'];
+// echo $s3_bucket = $_SERVER['S3_BUCKET'];
+
+// print_r($_ENV);
+
+
+?>
