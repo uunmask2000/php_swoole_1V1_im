3ff46a517a2a90971fb1a2d9589a36e3fda52d08
jim <chuang.jim@baiyitech.com.tw>
2020-05-07 14:12:48 +0800

    FIX CORS

--- Api.php
+++ Api.php
@@ -2,6 +2,10 @@
 <?php
 ## UTF-8
 header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
+
 ## load composer
 include 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
